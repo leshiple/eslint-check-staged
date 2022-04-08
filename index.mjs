acf280ce1185a84bd40977aa65ccfce2c902b529
alexandr.epihin <alexandr.epihin@sl-tech.ru>
2022-04-08 22:05:23 +0300

    Added chalk

--- index.mjs
+++ index.mjs
@@ -1,4 +1,27 @@
 #!/usr/bin/env node
-import { getStagedFiles } from './getStagedFiles'
+import path from 'path'
+import { getStagedFiles } from './getStagedFiles.mjs'
+
 
 const stagedFiles = await getStagedFiles()
+
+const pathToEslintConfig = path.join(process.cwd(), '.eslintrc.js')
+
+const config = await import(pathToEslintConfig)
+
+const targetFiles = []
+
+config?.default?.overrides?.forEach((override) => {
+  override?.files.forEach((file) => {
+    const isTarget = stagedFiles.some((f) => f.includes(file) || file.includes(f))
+
+    if (isTarget) {
+      targetFiles.push(file)
+    }
+  })
+})
+
+
+if(targetFiles.length) {
+  process.exit(1);
+}
